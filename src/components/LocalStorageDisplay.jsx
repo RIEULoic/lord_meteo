a7c8cbb19b0385c1688c6bdafbe1199ab8928d1e
RIEULoic <lrieu31@outlook.fr>
2023-05-17 14:16:06 +0400

    add localstoragedisplay component

--- src/components/LocalStorageDisplay.jsx
+++ src/components/LocalStorageDisplay.jsx
@@ -1,2 +1,34 @@
-const LocalStorageDisplay = () => <h1>local display</h1>;
+import { Link } from "react-router-dom";
+
+const LocalStorageDisplay = () => {
+  const citiesSavedOnLocalStorage = [];
+  for (let i = 1; i <= localStorage.length; i++) {
+    citiesSavedOnLocalStorage.push(JSON.parse(localStorage.getItem(i)));
+    //console.log(citiesSavedOnLocalStorage);
+  }
+  console.log(citiesSavedOnLocalStorage);
+
+  return (
+    <>
+      <h1>Précédentes recherches : </h1>
+      <div>
+        {citiesSavedOnLocalStorage.map((city) => {
+          console.log(city.cityIndex);
+          return (
+            <>
+              <button key={city.cityIndex}>
+                <Link
+                  to={`/city/${city.name.toLowerCase().replace(/\s/g, "")}`}
+                >
+                  <h1>{city.name}</h1>
+                </Link>
+              </button>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
 export default LocalStorageDisplay;
