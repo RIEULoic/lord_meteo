d2a0d3019d7a920c251fae7b218dc11b928f3249
RIEULoic <lrieu31@outlook.fr>
2023-05-17 16:15:32 +0400

    functionnal localStorage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-const Home = ({ citiesData, localStorageIndex, setLocalStorageIndex }) => {
+const Home = ({
+  citiesData,
+  localStorageIndex,
+  setLocalStorageIndex,
+  resetCitiesData,
+  setResetCitiesData,
+}) => {
   console.log(citiesData);
 
   function handleSaveLocalStorage(cityData) {
@@ -10,8 +16,10 @@ const Home = ({ citiesData, localStorageIndex, setLocalStorageIndex }) => {
       cityIndex: (localStorageIndex % 5) + 1,
       //Le modulo du cul permet que l'index ne dépasse pas 5. Si l'index dépasse 5, on revient à 1.
       name: cityData.name,
-      latitude: cityData.geoCode.latitude,
-      longitude: cityData.geoCode.longitude,
+      geoCode: {
+        latitude: cityData.geoCode.latitude,
+        longitude: cityData.geoCode.longitude,
+      },
     };
 
     localStorage.setItem(cityToSave.cityIndex, JSON.stringify(cityToSave));
@@ -19,30 +27,47 @@ const Home = ({ citiesData, localStorageIndex, setLocalStorageIndex }) => {
 
     //setLocalStorageIndex(localStorageIndex + 1);
     setLocalStorageIndex((prevIndex) => prevIndex + 1);
+    //setLocalStorageIndex((prevIndex) => prevIndex + 1) permet de récupérer la valeur précédente de localStorageIndex et de l'utiliser dans le setLocalStorageIndex. Si on utilise setLocalStorageIndex(localStorageIndex + 1), on récupère la valeur actuelle de localStorageIndex, qui est 0, et on lui ajoute 1. Donc on a toujours 1.
   }
 
   return (
     <div>
       <h1>Home</h1>
       <div>
-        {citiesData.map((city) => (
-          <button
-            key={
-              city.geoCode.latitude !== undefined ||
-              city.geoCode.longitude !== undefined
-                ? `${city.geoCode.latitude} ${
-                    city.geoCode.longitude
-                  } ${Math.random()}`
-                : `${Math.random()}`
+        {console.log(citiesData)}
+        {citiesData ? (
+          citiesData.map((city) => {
+            let cityInLocalStorage = false;
+            for (let i = 1; i <= 5; i++) {
+              const storedCity = JSON.parse(localStorage.getItem(i));
+              if (storedCity && storedCity.name === city.name) {
+                cityInLocalStorage = true;
+              }
             }
-            onClick={() => handleSaveLocalStorage(city)}
-            //Si tu écris onClick={handleSaveLocalStorage(city)}, la fonction s'exécute tout le temps
-          >
-            <Link to={`/city/${city.name.toLowerCase().replace(/\s/g, "")}`}>
-              {city.name}
-            </Link>
-          </button>
-        ))}
+            return cityInLocalStorage ? null : (
+              <button
+                key={
+                  city.geoCode?.latitude !== undefined ||
+                  city.geoCode?.longitude !== undefined
+                    ? `${city.geoCode.latitude} ${
+                        city.geoCode.longitude
+                      } ${Math.random()}`
+                    : `${Math.random()}`
+                }
+                onClick={() => handleSaveLocalStorage(city)}
+                //Si tu écris onClick={handleSaveLocalStorage(city)}, la fonction s'exécute tout le temps
+              >
+                <Link
+                  to={`/city/${city.name.toLowerCase().replace(/\s/g, "")}`}
+                >
+                  {city.name}
+                </Link>
+              </button>
+            );
+          })
+        ) : (
+          <h1>Aucunes villes trouvées</h1>
+        )}
       </div>
     </div>
   );
